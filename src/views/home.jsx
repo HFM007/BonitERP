e105891db9595be162cc964f95f8232b56c2160a
HFM007 <Habib.fauz43@gmail.com>
2025-05-14 00:16:29 +0700

    Add Home component with layout and welcome message

---
+++ src/views/home.jsx
@@ -0,0 +1,10 @@
+import Layout from "../views/layout/layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple home page.</p>
+    </Layout>
+  )
+}
