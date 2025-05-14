ef2f87ff91940c9588b47e69ce38c151321058bd
HFM007 <Habib.fauz43@gmail.com>
2025-05-14 20:00:44 +0700

    style: refactor Layout component for improved structure and Tailwind CSS integration

--- src/views/layout/layout.jsx
+++ src/views/layout/layout.jsx
@@ -4,15 +4,15 @@ import Footer from "./footer";
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Sidebar />
-      <div>
-        <Header />
-        <main>
+    <div className="flex flex-col h-screen w-screen overflow-hidden">
+      <Header className="w-full" />
+      {/* <div className="flex flex-1 overflow-hidden">
+        <Sidebar className="w-64 bg-base-300" />
+      </div> */}
+        <main className="flex-1 p-4 bg-base-100 overflow-auto">
           {children}
         </main>
-        <Footer />
-      </div>
+      <Footer className="w-full" />
     </div>
   );
 }
