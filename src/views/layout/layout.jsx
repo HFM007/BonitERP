45f1f3b978f51f467458dd409ba796bf61ce13d6
HFM007 <Habib.fauz43@gmail.com>
2025-05-14 21:29:48 +0700

    Remove commented-out Sidebar component from Layout for cleaner code

--- src/views/layout/layout.jsx
+++ src/views/layout/layout.jsx
@@ -6,9 +6,6 @@ export default function Layout({ children }) {
   return (
     <div className="flex flex-col h-screen w-screen overflow-hidden">
       <Header className="w-full" />
-      {/* <div className="flex flex-1 overflow-hidden">
-        <Sidebar className="w-64 bg-base-300" />
-      </div> */}
         <main className="flex-1 p-4 bg-base-100 overflow-auto">
           {children}
         </main>
