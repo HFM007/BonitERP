c71b78d464d2888d0ed3d937c600283c49376c44
HFM007 <Habib.fauz43@gmail.com>
2025-05-14 21:30:31 +0700

    Update Sidebar component structure and styling for improved layout

--- src/views/layout/sidebar.jsx
+++ src/views/layout/sidebar.jsx
@@ -1,20 +1,21 @@
 export default function Sidebar() {
   return (
-    <nav className="h-screen w-64 bg-gray-800 text-white">
-      <ul className="menu p-4 w-full">
-        <li className="mb-2">
-          <a href="#" className="hover:bg-gray-700">Dashboard</a>
+    <ul
+      tabIndex={0}
+      className="menu menu-lg dropdown-content bg-base-100 rounded-box z-1 mt-3 w-64 p-4 shadow">
+      <ul className="menu bg-base-200 rounded-box w-56">
+        <li><a>Item 1</a></li>
+        <li>
+          <details open>
+            <summary className="">Parent</summary>
+            <ul>
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </details>
         </li>
-        <li className="mb-2">
-          <a href="#" className="hover:bg-gray-700">Profile</a>
-        </li>
-        <li className="mb-2">
-          <a href="#" className="hover:bg-gray-700">Settings</a>
-        </li>
-        <li className="mb-2">
-          <a href="#" className="hover:bg-gray-700">Logout</a>
-        </li>
+        <li><a>Item 3</a></li>
       </ul>
-    </nav>
+    </ul>
   );
 }
