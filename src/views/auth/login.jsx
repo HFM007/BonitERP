adf0138204dafe16f57e59c953eb7de8c4fdde11
HFM007 <Habib.fauz43@gmail.com>
2025-05-17 18:17:25 +0700

    Add Login component with form for user authentication

---
+++ src/views/auth/login.jsx
@@ -0,0 +1,82 @@
+import Layout from "../layout/layout";
+
+export default function Login() {
+  return (
+    <Layout>
+      <div className="flex items-center justify-center min-h-[calc(100vh-12rem)] bg-base-100 px-4 sm:px-6 lg:px-8">
+        <div className="hero-content text-center w-full max-w-md">
+          <div className="card bg-base-100 image-full shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Login BonitERP</h2>
+              <p className="text-sm">Silahkan masukan Email dan Password untuk login!</p>
+              <label className="label">Email</label>
+              <label className="input validator w-full">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                  </g>
+                </svg>
+                <input type="email" placeholder="mail@site.com" className="w-full" required />
+              </label>
+              <div className="validator-hint hidden">Enter valid email address</div>
+              <label className="label">Password</label>
+              <label className="input validator w-full">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path
+                      d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+                    ></path>
+                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                  </g>
+                </svg>
+                <input
+                  type="password"
+                  required
+                  placeholder="Password"
+                  className="w-full"
+                  minlength="8"
+                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                  title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                />
+              </label>
+              <p className="validator-hint hidden">
+                Must be more than 8 characters, including
+                <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+              </p>
+              <div className="card-actions justify-center">
+                <button className="btn btn-primary w-full sm:w-auto">Login</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
